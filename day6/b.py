ce1b14e7280c0504a50b3c67556f52a25eedf300
Cefqrn <Cefqrn@gmail.com>
2021-12-06 00:54:43 -0500

    added day6/b.py

---
+++ day6/b.py
@@ -0,0 +1,25 @@
+import os
+from collections import deque
+
+with open(os.path.join(os.path.dirname(__file__), "input")) as f:
+    *data, = map(int, f.read().strip().split(','))
+
+fish = deque([0,0,0,0,0,0,0,0,0], maxlen=9)
+for x in data:
+    fish[x] += 1
+
+eight = 0
+seven = 0
+for i in range(80):
+    fish.rotate(-1)
+    fish[6] += fish[8]
+
+# part 1
+print(sum(fish)+seven+eight)
+
+for i in range(256-80):
+    fish.rotate(-1)
+    fish[6] += fish[8]
+
+# part 2
+print(sum(fish)+seven+eight)
